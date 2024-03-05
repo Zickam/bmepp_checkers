6fe76580a602938712b0a0d3cbbccbb9b06e0e35
Игорь <iantuganov@yandex.ru>
2024-03-06 00:58:00 +0500

    main.py: structure, drawing board

--- constants.py
+++ constants.py
@@ -1,1 +1,6 @@
-WIN_SIZE = (700, 700)
+WIN_SIZE = (880, 800)
+FPS = 60
+BCC = (55, 140, 15)  # black cells color
+WCC = (230, 230, 230)  # white cells color
+BBC = (20, 20, 20)  # board border color
+CELLS_NUMBER = 8
