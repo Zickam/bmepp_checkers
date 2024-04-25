9bdeb2d3bf4c4011cdb91e3c21115e3e3dc96251
Deshp666 <motvey74@mail.ru>
2024-04-25 19:44:33 +0500

    Class Result added, Tournament init and cool to-do list :)

---
+++ competition/classes.py
@@ -0,0 +1,74 @@
+class Weights:
+    @staticmethod
+    def str_to_weights(str_weights: str) -> list[float]:
+        str_weights.replace('\n', '')
+        try:
+            str_list = str_weights.split(' ')
+            float_weights = []
+            for weight in str_list:
+                float_weights.append(float(weight))
+            return float_weights
+        except ValueError as ex:
+            print(ex, 'у вас весах не то:', str_weights)
+
+    @staticmethod
+    def weights_to_str(float_weights: list[float]) -> str:
+        str_weights = ''
+        for weight in float_weights:
+            str_weights += str(weight) + ' '
+        return str_weights[:-1]
+
+    @staticmethod
+    def create_generation(weights: list[list[float]]):
+        pass
+
+    @staticmethod
+    def load_weights_from_file(filename) -> list[list[float]]:
+        strings = open(filename, 'r').readlines()
+        weights = []
+        for s in strings:
+            weights.append(Weights.str_to_weights(s))
+        return weights
+
+    @staticmethod
+    def save_weights_in_file(filename: str, weights: list[list[float]]):
+        with open(filename, 'w') as file:
+            for weight in weights:
+                str_weights = Weights.weights_to_str(weight) + '\n'
+                file.write(str_weights)
+
+
+class Result:
+    @staticmethod
+    def str_to_results(str_results: str) -> list[int]:
+        str_results.replace('\n', '')
+        try:
+            str_list = str_results.split(' ')
+            int_results = []
+            for results in str_list:
+                int_results.append(int(results))
+            return int_results
+        except ValueError as ex:
+            print(ex, 'у вас в резах не то:', str_results)
+
+    @staticmethod
+    def results_to_str(int_results: list[int]) -> str:
+        str_results = ''
+        for results in int_results:
+            str_results += str(results) + ' '
+        return str_results[:-1]
+
+    @staticmethod
+    def load_results_from_file(filename) -> list[list[int]]:
+        strings = open(filename, 'r').readlines()
+        results = []
+        for s in strings:
+            results.append(Result.str_to_results(s))
+        return results
+
+    @staticmethod
+    def save_results_in_file(filename: str, results: list[list[int]]):
+        with open(filename, 'w') as file:
+            for result in results:
+                str_results = Result.weights_to_str(result) + '\n'
+                file.write(str_results)
--- competition/main.py
+++ competition/main.py
@@ -1,22 +1,26 @@
 from game.bot import Bot
-from competition.weights import Weights
+from competition.classes import Weights
 from constants import *
+import multiprocessing as mp
 
 # TO DO:
-# Веса из файла
-# Сохранение результатов
-# Проведение матчей,                        потом мутации
+# Веса из файла ✅
+# Боты из весов ❌
+# Сохранение результатов в файл ✅
+# Проведение матчей ❌,                        потом мутации❌
 
+# разделение игры на этапы ❓
+# запоминать блоки весов, которые остаются n раундов ❓
 
 class Tournament:
     def __init__(self):
-        pass
+        self.results: list[list[int]]
+        self.generation_number: int
+        self.weights: list[list[int]]
+        self.bots: list[Bot]
+        self.duel_pairs_queue: mp.Queue
+        self.duel_results_queue: mp.Queue
+        # self.mainloop()
 
-    @staticmethod
-    def load_weights_from_file(filename):
-        strings = open(filename, 'r').readlines()
-        weights = []
-        for s in strings:
-            weights.append(Weights.str_to_weights(s))
-        for w in weights:
-            print(w)
+    def save_result(self):
+        pass
