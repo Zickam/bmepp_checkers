9e991a8de16e8a0d3997972e9ff10bfc6aeeae81
Игорь <iantuganov@yandex.ru>
2024-05-02 22:45:49 +0500

    requirements.txt updated

--- competition/constants.py
+++ competition/constants.py
@@ -1,6 +1,6 @@
 PARALLEL_MATCHES = 6
 PATH_TO_DATA = 'competition/data/'
 OPPONENTS_COUNT = 5
-WEIGHTS_COUNT = 15
+WEIGHTS_COUNT = 15  # at least OPPONENTS_COUNT * 2 + 1
 # coefficient for fitness function
 ALPHA, BETA, GAMMA = 10, 1, -100
